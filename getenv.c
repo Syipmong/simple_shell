78001d036dbbe3bb08ca998d9a51f7fcb0a7c028
Anphimens <philipinamensah19@gmail.com>
2023-07-26 11:26:14 -0700

    A program to get the environment variable

---
+++ getenv.c
@@ -0,0 +1,49 @@
+#include "main1.h"
+#include "shell1.h"
+
+/**
+ * get_env - a program to get the values of PATH environment variable
+ * @command: command passed as argument
+ *
+ * Return: A pointer to string
+ */
+char *get_env(char *command)
+{
+	char *path, *path_copy, *path_token, *file_path;
+	int command_length, directory_length;
+	struct stat buffer;
+
+	path = getenv("PATH");
+	if (path)
+	{
+		path_copy = strdup(path);
+		command_length = _strlen(command);
+		path_token = strtok(path_copy, ":");
+
+		while (path_token != NULL)
+		{
+			directory_length = _strlen(path_token);
+			file_path = malloc(command_length + directory_length + 2);
+			_strcpy(file_path, path_token);
+			_strcat(file_path, "/");
+			_strcat(file_path, command);
+			_strcat(file_path, "\0");
+
+			if (stat(file_path, &buffer) == 0)
+			{
+				free(path_copy);
+				return (file_path);
+			}
+			else
+			{
+				free(file_path);
+				path_token = strtok(NULL, ":");
+			}
+		}
+		free(path_copy);
+		if (stat(command, &buffer) == 0)
+			return (command);
+		return (NULL);
+	}
+	return (NULL);
+}
