3dae8dd776fe2b7c0dcfeedc19fdfb2705df3306
Anphimens <philipinamensah19@gmail.com>
2023-07-26 11:29:28 -0700

    Header number 2 containing prototypes of other functions

---
+++ shell1.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+void freewords(char **commands);
+
+#endif
